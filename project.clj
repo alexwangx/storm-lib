95dbee7d6ed7ef9258e026250a98579371f17a89
alexwang <hello123.cn@gmail.com>
2018-07-08 22:57:20 +0800

    a

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject storm-lib "1.0.0-SNAPSHOT"
+  :description "storm-lib"
+  :source-path "src/clj"
+  :java-source-path "src/jvm"
+  :dependencies [[org.clojure/clojure "1.5.0"]
+                 [storm/storm "0.9.0-rc2"]]
+  :aot :all)
